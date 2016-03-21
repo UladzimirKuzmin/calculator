6119f238025d57a2f14b7d8e102b61c440cf517b
UladzimirKuzmin <Vladimir.Kuzmin@effective-soft.com>
2016-03-21 11:32:41 +0300

    fix

--- demo/js/config.js
+++ demo/js/config.js
@@ -1,7 +1,7 @@
 require.config({ //jshint ignore:line
 	paths: {
-        lodash: '/calculator/demo/lodash',
-        app: '/calculator/demo/app'
+        lodash: '/calculator/demo/js/lodash',
+        app: '/calculator/demo/js/app'
     },
     shim: {
         'app': {
