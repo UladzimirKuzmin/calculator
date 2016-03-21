12ea05e6ffc34e15c559ea3f270188ef62d919e9
UladzimirKuzmin <Vladimir.Kuzmin@effective-soft.com>
2016-03-21 10:54:42 +0300

    fix

--- demo/js/config.js
+++ demo/js/config.js
@@ -1,5 +1,5 @@
 require.config({ //jshint ignore:line
-	baseUrl: 'http://localhost:8888',
+	baseUrl: 'http://uladzimirkuzmin.github.io/calculator/demo/',
     paths: {
         lodash: '/js/lodash',
         app: '/js/app'
